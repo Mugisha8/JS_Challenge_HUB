90520cd2823f19b21e97f38d5c45761aad1ec90a
Mugisha8 <yvesmugisha8@gmail.com>
2022-12-26 15:37:12 +0200

    challenge 001

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1997,3 +1997,31 @@
 // }
 
 // greet.sayHi(); // Jack
+
+
+
+const person = {
+    name : 'Jack',
+    age: 25,
+
+    // this inside method
+    // this refers to the object itself
+    greet() {
+        console.log(this);
+        console.log(this.age);
+
+        // inner function
+        let innerFunc = () => {
+        
+            // this refers to the global object
+            console.log(this);
+            console.log(this.age);
+            
+        }
+
+        innerFunc();
+
+    }
+}
+
+person.greet();
