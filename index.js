2a58066db955546de0b1142694a51f37ade40467
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-24 10:51:43 -1000

    numbers input

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const num1 = 2;
-const num2 = 3;
+// const num1 = 2;
+// const num2 = 3;
 
-const sum = num1 + num2;
+// const sum = num1 + num2;
 
-console.log("The sum of " + num1 + " and " + num2 + "is" , sum);
+// console.log("The sum of " + num1 + " and " + num2 + "is" , sum);
+
+
+
+const readline = require('readline');
+
+const add = readline.createInterface({  
+    input:process.stdin,
+    output:process.stdout
+})
+
+add.question('enter the firstquestion',(num1)=>{
+add.question('enter the second number',(num2)=>{
+
+    num1=parseInt(num1);
+    num2=parseInt(num2);
+    
+    const sum = num1 + num2
+
+    console.log(`the sum of ${num1} and ${num2} is ${sum}`);
+
+
+
+})
+
+})
