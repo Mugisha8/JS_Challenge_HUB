4a6612134c4223051cb8929e28f0185ba3aecf7a
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-25 23:59:35 +0200

    challenge 20

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -233,3 +233,17 @@ for (let i = 1; i <= 5; i++) {
 
     console.log(i);
 }
+
+// nested for loops
+
+// first loop
+for (let i = 1; i <= 3; i++) {
+
+    // second loop
+    for (let j = 1; j <= 3; j++) {
+        if (j == 2) {
+          continue;
+        }
+        console.log(`i = ${i}, j = ${j}`);
+    }
+}
