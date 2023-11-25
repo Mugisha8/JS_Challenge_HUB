bd29fbad797c86ba37651605ecd05ada5c2207b7
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-25 23:56:53 +0200

    challenge 17

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -196,3 +196,13 @@
 // }
 // while(i<=10)
 // console.log(sum)
+
+
+// program to print the value of i
+for (let i = 1; i <= 5; i++) {
+    // break condition     
+    if (i == 3) {
+        break;
+    }
+    console.log(i);
+}
