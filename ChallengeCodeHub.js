d716ffe11a73a39bf32504a618c093e484bd2d6a
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-26 00:03:35 +0200

    challenge 21

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -209,41 +209,70 @@
 
 
 
-// nested for loops
+// // nested for loops
 
-// first loop
-for (let i = 1; i <= 3; i++) {
+// // first loop
+// for (let i = 1; i <= 3; i++) {
 
-    // second loop
-    for (let j = 1; j <= 3; j++) {
-        if (i == 2) {
-          break;
-        }
-        console.log(`i = ${i}, j = ${j}`);
-    }
-}
+//     // second loop
+//     for (let j = 1; j <= 3; j++) {
+//         if (i == 2) {
+//           break;
+//         }
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
 
-// program to print the value of i
-for (let i = 1; i <= 5; i++) {
+// // program to print the value of i
+// for (let i = 1; i <= 5; i++) {
 
-    // condition to continue    
-    if (i == 3) {
+//     // condition to continue    
+//     if (i == 3) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+// // nested for loops
+
+// // first loop
+// for (let i = 1; i <= 3; i++) {
+
+//     // second loop
+//     for (let j = 1; j <= 3; j++) {
+//         if (j == 2) {
+//           continue;
+//         }
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+
+// program to calculate positive numbers only
+// if the user enters a negative number, that number is skipped from calculation
+
+// negative number -> loop terminate
+// non-numeric character -> skip iteration
+
+let sum = 0;
+let number = 0;
+
+while (number >= 0) {
+
+    // add all positive numbers
+    sum += number;
+
+    // take input from the user
+    number = parseInt(prompt('Enter a number: '));
+
+    // continue condition
+    if (isNaN(number)) {
+        console.log('You entered a string.');
+        number = 0; // the value of number is made 0 again
         continue;
     }
 
-    console.log(i);
 }
 
-// nested for loops
-
-// first loop
-for (let i = 1; i <= 3; i++) {
-
-    // second loop
-    for (let j = 1; j <= 3; j++) {
-        if (j == 2) {
-          continue;
-        }
-        console.log(`i = ${i}, j = ${j}`);
-    }
-}
+// display the sum
+console.log(`The sum is ${sum}.`);
