427622a36dfd5a9e612a0fd459a76d817d6dbd93
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-25 23:57:43 +0200

    challenge 18

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -198,11 +198,27 @@
 // console.log(sum)
 
 
-// program to print the value of i
-for (let i = 1; i <= 5; i++) {
-    // break condition     
-    if (i == 3) {
-        break;
+// // program to print the value of i
+// for (let i = 1; i <= 5; i++) {
+//     // break condition     
+//     if (i == 3) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+
+// nested for loops
+
+// first loop
+for (let i = 1; i <= 3; i++) {
+
+    // second loop
+    for (let j = 1; j <= 3; j++) {
+        if (i == 2) {
+          break;
+        }
+        console.log(`i = ${i}, j = ${j}`);
     }
-    console.log(i);
 }
