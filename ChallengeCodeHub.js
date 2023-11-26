6b59b6eefeb3bc53861f365d296790dc82831179
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-26 22:41:47 +0200

    challenge 30

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -312,9 +312,20 @@
 // declaring a function named greet()
 // program to print a text
 // declaring a function
-function greet() {
-    console.log("Hello there!");
+// function greet() {
+//     console.log("Hello there!");
+// }
+
+// // calling the function
+// greet();
+
+
+// program to add two numbers using a function
+// declaring a function
+function add(a, b) {
+    console.log(a + b);
 }
 
-// calling the function
-greet();
+// calling functions
+add(3,4);
+add(2,9);
