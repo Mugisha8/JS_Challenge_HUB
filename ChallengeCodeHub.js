839cedb24f478305c845c8b67120abad7619c5a9
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-26 22:01:00 +0200

    update

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -254,25 +254,26 @@
 // negative number -> loop terminate
 // non-numeric character -> skip iteration
 
-let sum = 0;
-let number = 0;
+// let sum = 0;
+// let number = 0;
 
-while (number >= 0) {
+// while (number >= 0) {
 
-    // add all positive numbers
-    sum += number;
+//     // add all positive numbers
+//     sum += number;
 
-    // take input from the user
-    number = parseInt(prompt('Enter a number: '));
+//     // take input from the user
+//     number = parseInt(prompt('Enter a number: '));
 
-    // continue condition
-    if (isNaN(number)) {
-        console.log('You entered a string.');
-        number = 0; // the value of number is made 0 again
-        continue;
-    }
+//     // continue condition
+//     if (isNaN(number)) {
+//         console.log('You entered a string.');
+//         number = 0; // the value of number is made 0 again
+//         continue;
+//     }
 
-}
+// }
 
-// display the sum
-console.log(`The sum is ${sum}.`);
+// // display the sum
+// console.log(`The sum is ${sum}.`);
+
