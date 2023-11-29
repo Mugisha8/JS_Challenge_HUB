b778edc54b7ff7a2b68a1290f6b3271e2aefe12f
Mugisha8 <yvesmugisha8@gmail.com>
2023-11-29 11:24:27 +0200

    challenge 50

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -490,25 +490,38 @@
 // console.log("Am From"+" "+navigation.name + " " + navigation.code);
 
 // constructor function
-function Person () {
-    this.name = 'John',
-    this.age = 23
+// function Person () {
+//     this.name = 'John',
+//     this.age = 23
+// }
+
+// // creating objects
+// let person1 = new Person();
+// let person2 = new Person();
+
+// // adding property to person1 object
+// person1.gender = 'male';
+
+// // adding method to person1 object
+// person1.greet = function () {
+//     console.log('hello');
+// }
+
+// person1.greet();   // hello
+
+// // Error code
+// // person2 doesn't have greet() method
+// person2.greet();
+
+
+
+
+const student = {
+    firstname:"yves",
+
+    get information() {
+return this.firstname;
+    }
 }
 
-// creating objects
-let person1 = new Person();
-let person2 = new Person();
-
-// adding property to person1 object
-person1.gender = 'male';
-
-// adding method to person1 object
-person1.greet = function () {
-    console.log('hello');
-}
-
-person1.greet();   // hello
-
-// Error code
-// person2 doesn't have greet() method
-person2.greet();
+console.log(student.information)
