a7fedc6b0c42f56c99520ab3fcb575c8da276249
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-04 23:08:04 +0200

    challenge 65

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -638,5 +638,19 @@
 
 // console.log("10! =", result);
 
-console.log(1 === true)
+// console.log(1 === true)
+
+
+const student = {
+    name: 'Monica',
+    class: 7,
+    age: 12
+}
+
+// using for...in
+for ( let key in student ) {
+
+    // display the properties
+    console.log(`${key} => ${student[key]}`);
+}
 
