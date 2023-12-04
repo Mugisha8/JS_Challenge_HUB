7abee9b73b60cd7d677360a165c53f71f8a52c2b
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-04 23:17:37 +0200

    cahllenge 70

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -671,3 +671,8 @@
 // for (let x in arr) {
 //     console.log(arr[x]);
 // }
+
+//strings example
+const name = 'Peter';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`;
