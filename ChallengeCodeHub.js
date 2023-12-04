12d7e3369aea23c63ef14397f44787aeecef5431
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-04 23:12:14 +0200

    component updt

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -654,19 +654,4 @@
 //     console.log(`${key} => ${student[key]}`);
 // }
 
-const salaries= {
-    Jack : 24000,
-    Paul : 34000,
-    Monica : 55000
-}
-
-// using for...in
-for ( let i in salaries) {
-
-    // add a currency symbol
-    let salary = "$" + salaries[i];
-
-    // display the values
-    console.log(`${i} : ${salary}`);
-}
 
