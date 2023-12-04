b9919fc831551b0e8c61d009c227f3a98cc741de
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-04 23:19:18 +0200

    updt

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -673,6 +673,6 @@
 // }
 
 //strings example
-const name = 'Peter';
-const name1 = "Jack";
-const result = `The names are ${name} and ${name1}`;
+// const name = 'Peter';
+// const name1 = "Jack";
+// const result = `The names are ${name} and ${name1}`;
