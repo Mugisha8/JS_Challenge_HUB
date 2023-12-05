4ff21718cff8faa893bedde68a9b1b75e6a4e33f
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-05 23:18:32 +0200

    challenge 80

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -706,3 +706,18 @@
 // console.log(person.id); // Another value
 
 // 
+
+
+const numerator= 100, denominator = 'a';
+
+try {
+     console.log(numerator/denominator);
+     console.log(a);
+}
+catch(error) {
+    console.log('An error caught'); 
+    console.log('Error message: ' + error);  
+}
+finally {
+     console.log('Finally will execute every time');
+}
