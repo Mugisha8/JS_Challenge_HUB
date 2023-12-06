721824590b3369d570c83d5cf851b7e039d93b97
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-06 23:20:30 +0200

    challenge 86

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -781,6 +781,14 @@
 // }
 
 
-// 
+// // variable declared using let
+let name = 'Sara';
+{
+    // can be accessed only inside
+    let name = 'Peter';
+
+    console.log(name); // Peter
+}
+console.log(name); // Sara 
 
 
