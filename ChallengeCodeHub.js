e893cc06764618d7560f88d980924afa28cc2876
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-06 23:26:19 +0200

    challenge 91

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -805,3 +805,13 @@
 //   const person1 = new Person('John');
   
 //   console.log(person1.name); // John
+
+function sum(x, y = 5) {
+
+    // take sum
+    // the value of y is 5 if not passed
+    console.log(x + y);
+}
+
+sum(5); // 10
+sum(5, 15); // 20
