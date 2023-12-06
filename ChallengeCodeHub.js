05d9231dceb6eedf464d381d93b3362ae3c806c0
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-06 23:24:24 +0200

    challenge 90

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -796,3 +796,12 @@
 // function expression
 // 
 
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const person1 = new Person('John');
+  
+  console.log(person1.name); // John
