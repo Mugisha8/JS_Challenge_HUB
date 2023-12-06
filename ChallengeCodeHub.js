12db8bfbe7da768d918fcd0ae7e061474e3ffa42
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-06 23:25:06 +0200

    updt

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -796,12 +796,12 @@
 // function expression
 // 
 
-class Person {
-    constructor(name) {
-      this.name = name;
-    }
-  }
+// class Person {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
   
-  const person1 = new Person('John');
+//   const person1 = new Person('John');
   
-  console.log(person1.name); // John
+//   console.log(person1.name); // John
