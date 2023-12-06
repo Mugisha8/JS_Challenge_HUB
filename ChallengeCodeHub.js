9b23ae969052a397b175e549c2a5538f3c3e85a9
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-06 23:12:15 +0200

    component updt

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -737,24 +737,26 @@
 //         console.log( "error is caught" );
 //     }
 // }, 3000);
-const number = 40;
-try {
-    if(number > 50) {
-        console.log('Success');
-    }
-    else {
 
-        // user-defined throw statement
-        throw new Error('The number is low');
-    }
 
-    // if throw executes, the below code does not execute
-    console.log('hello');
-}
-catch(error) {
-    console.log('An error caught'); 
-    console.log('Error message: ' + error);  
-}
+// const number = 40;
+// try {
+//     if(number > 50) {
+//         console.log('Success');
+//     }
+//     else {
+
+//         // user-defined throw statement
+//         throw new Error('The number is low');
+//     }
+
+//     // if throw executes, the below code does not execute
+//     console.log('hello');
+// }
+// catch(error) {
+//     console.log('An error caught'); 
+//     console.log('Error message: ' + error);  
+// }
 
 
 
