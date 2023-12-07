a0c315fd769683fdc812dabeb32a509e6d3330dc
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-07 21:35:32 +0200

    challenge 98

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -857,3 +857,11 @@
 //   () => console.log('Adult');
 
 // welcome(); // Baby
+
+let sum = (a, b) => {
+    let result = a + b;
+    return result;
+}
+
+let result1 = sum(5,7);
+console.log(result1); // 12
