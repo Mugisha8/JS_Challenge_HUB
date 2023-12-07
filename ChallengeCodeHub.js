b917e8e50cec600b849ccf0c6985d4ebe0d32922
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-07 21:31:35 +0200

    challenge 97

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -849,3 +849,11 @@
 
 // let greet = () => console.log('Hello');
 // greet(); // Hello
+
+let age = 5;
+
+let welcome = (age < 18) ?
+  () => console.log('Baby') :
+  () => console.log('Adult');
+
+welcome(); // Baby
