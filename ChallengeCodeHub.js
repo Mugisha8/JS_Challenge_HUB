0a4f0db459c5f631e32234521c1ce94f901b1da8
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-07 21:42:00 +0200

    challenge 100

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -889,3 +889,20 @@
 
 // let x = new Person();
 // x.sayName();
+
+function Person() {
+    this.name = 'Jack',
+    this.age = 25,
+    this.sayName = function () {
+
+        console.log(this.age);
+        let innerFunc = () => {
+            console.log(this.age);
+        }
+
+        innerFunc();
+    }
+}
+
+const x = new Person();
+x.sayName();
