88095aa53a884995418c6864d8a327a65aa23464
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-08 23:38:59 +0200

    challenge 117

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -948,3 +948,21 @@
 // const result = tagExample('Hello Jack');
 
 // console.log(result);
+
+const name = 'Jack';
+const greet = true;
+
+function tagExample(strings, nameValue) {
+    let str0 = strings[0]; // Hello
+    let str1 = strings[1]; // , How are you?
+
+    if(greet) {
+        return `${str0}${nameValue}${str1}`;
+    }
+}
+
+// creating tagged literal
+// passing argument name
+const result = tagExample`Hello ${name}, How are you?`;
+
+console.log(result);
