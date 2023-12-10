86a8d1e9458444105bcdbc88b6ef4694cfaef99e
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:48:48 +0200

    challenge 125

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1033,3 +1033,12 @@
 // const num1 = [1, 3, 4, 5];
 
 // sum(...num1); // 8
+
+
+// create a set
+let map1 = new Map();
+
+// insert key-value pair
+map1.set('info', {name: 'Jack', age: 26});
+console.log(map1); // Map {"info" => {name: "Jack", age: 26}}
+
