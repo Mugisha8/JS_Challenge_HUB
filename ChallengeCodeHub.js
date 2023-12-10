d76de8874c8a5aa11a678b00c74c3c11c7bb7638
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:59:00 +0200

    component updted

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1067,11 +1067,11 @@
 // map1.delete('info'); // true
 // console.log(map1); // Map {}
 
-let map1 = new Map();
-map1.set('name', 'Jack');
-map1.set('age', '27');
+// let map1 = new Map();
+// map1.set('name', 'Jack');
+// map1.set('age', '27');
 
-// looping through Map
-for (let [key, value] of map1) {
-    console.log(key + '- ' + value);
-}
+// // looping through Map
+// for (let [key, value] of map1) {
+//     console.log(key + '- ' + value);
+// }
