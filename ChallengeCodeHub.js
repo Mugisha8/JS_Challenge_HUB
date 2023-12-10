162dd081680ac7ad32145036e3ac9114b757c979
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:56:21 +0200

    challenge 128

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1056,3 +1056,13 @@
 
 // // check if an element is in Set
 // console.log(map1.has('info')); // true
+
+let map1 = new Map();
+map1.set('info', {name: 'Jack', age: "26"});
+
+// removing a particular element
+map1.delete('address'); // false
+console.log(map1); // Map {"info" => {name: "Jack", age: "26"}} 
+
+map1.delete('info'); // true
+console.log(map1); // Map {}git
