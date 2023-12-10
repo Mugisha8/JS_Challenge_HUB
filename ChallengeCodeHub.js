c3dfa06c51e7d1f82b16fc3d6293d10f3c62217f
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:56:58 +0200

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1057,12 +1057,12 @@
 // // check if an element is in Set
 // console.log(map1.has('info')); // true
 
-let map1 = new Map();
-map1.set('info', {name: 'Jack', age: "26"});
+// let map1 = new Map();
+// map1.set('info', {name: 'Jack', age: "26"});
 
-// removing a particular element
-map1.delete('address'); // false
-console.log(map1); // Map {"info" => {name: "Jack", age: "26"}} 
+// // removing a particular element
+// map1.delete('address'); // false
+// console.log(map1); // Map {"info" => {name: "Jack", age: "26"}} 
 
-map1.delete('info'); // true
-console.log(map1); // Map {}git
+// map1.delete('info'); // true
+// console.log(map1); // Map {}
