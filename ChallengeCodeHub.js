e034a9613cc11b9abdad496acd1fb74bfeb2d511
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:52:34 +0200

    challenge 126

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1042,3 +1042,9 @@
 // map1.set('info', {name: 'Jack', age: 26});
 // console.log(map1); // Map {"info" => {name: "Jack", age: 26}}
 
+
+let map1 = new Map();
+map1.set('info', {name: 'Jack', age: "26"});
+
+// access the elements of a Map
+console.log(map1.get('info')); // {name: "Jack", age: "26"}
