3c32a36aa98bf47874770e08299207aa012512fa
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-10 23:58:24 +0200

    challenge 129

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1066,3 +1066,12 @@
 
 // map1.delete('info'); // true
 // console.log(map1); // Map {}
+
+let map1 = new Map();
+map1.set('name', 'Jack');
+map1.set('age', '27');
+
+// looping through Map
+for (let [key, value] of map1) {
+    console.log(key + '- ' + value);
+}
