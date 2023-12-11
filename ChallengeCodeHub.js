fad83b992f4796b7133317eabca63244df74e10d
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-11 23:55:59 +0200

    challenge 130

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1076,12 +1076,12 @@
 //     console.log(key + '- ' + value);
 // }
 
-// using forEach method()
-// let map1 = new Map();
-// map1.set('name', 'Jack');
-// map1.set('age', '27');
+using forEach method()
+let map1 = new Map();
+map1.set('name', 'Jack');
+map1.set('age', '27');
 
-// // looping through Map
-// map1.forEach(function(value, key) {
-//   console.log(key + '- ' + value)
-// })
+// looping through Map
+map1.forEach(function(value, key) {
+  console.log(key + '- ' + value)
+})
