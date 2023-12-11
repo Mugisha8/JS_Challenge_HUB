958b361b3fbb396347b82efacfe7e6769e93600b
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-11 23:59:15 +0200

    challenge 134

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1106,3 +1106,8 @@
 
 // // access the elements of a Set
 // console.log(set1.values()); // Set Iterator [1, 2, 3]
+
+const set1 = new Set([1, 2, 3]);
+
+// access the elements of a Set
+console.log(set1.values()); // Set Iterator [1, 2, 3]
