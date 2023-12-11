336203738721c0ec75a59c4b76f9e514887432eb
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-11 23:58:03 +0200

    challenge 133

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1096,3 +1096,7 @@
 // // Set with multiple types of value
 // const set2 = new Set([1, 'hello', {count : true}]);
 // console.log(set2); // Set {1, "hello", {count: true}}
+
+// Set with duplicate values
+const set3 = new Set([1, 1, 2, 2]);
+console.log(set3); // Set {1, 2}
