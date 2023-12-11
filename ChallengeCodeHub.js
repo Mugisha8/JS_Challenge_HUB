f65953e349148cceb2dbef3307aefc6eee83c7a3
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-11 23:58:51 +0200

    component updt

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1098,5 +1098,11 @@
 // console.log(set2); // Set {1, "hello", {count: true}}
 
 // Set with duplicate values
-const set3 = new Set([1, 1, 2, 2]);
-console.log(set3); // Set {1, 2}
+// const set3 = new Set([1, 1, 2, 2]);
+// console.log(set3); // Set {1, 2}
+
+
+// const set1 = new Set([1, 2, 3]);
+
+// // access the elements of a Set
+// console.log(set1.values()); // Set Iterator [1, 2, 3]
