c4f54f23ada2a5bf20742d29a06a6b5bbb6fdeb2
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-11 23:57:37 +0200

    challenge 132

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1089,10 +1089,10 @@
 
 //___________________ review
 
-// create Set
-const set1 = new Set(); // an empty set
-console.log(set1); // Set {}
+// // create Set
+// const set1 = new Set(); // an empty set
+// console.log(set1); // Set {}
 
-// Set with multiple types of value
-const set2 = new Set([1, 'hello', {count : true}]);
-console.log(set2); // Set {1, "hello", {count: true}}
+// // Set with multiple types of value
+// const set2 = new Set([1, 'hello', {count : true}]);
+// console.log(set2); // Set {1, "hello", {count: true}}
