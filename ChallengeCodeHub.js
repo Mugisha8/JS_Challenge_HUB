a7a72308947ec3b5bcba41ac3d115b773c4af567
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-13 00:02:11 +0200

    challenge using js destructuring

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1133,15 +1133,15 @@
 // console.log(gender); // female
 
 // assigning object attributes to variables
-const person = {
-    name: 'Sara',
-    age: 25,
-    gender: 'female'    
-}
+// const person = {
+//     name: 'Sara',
+//     age: 25,
+//     gender: 'female'    
+// }
 
-// destructuring assignment
-let { name, age, gender } = person;
+// // destructuring assignment
+// let { name, age, gender } = person;
 
-console.log(name); // Sara
-console.log(age); // 25
-console.log(gender); // female
+// console.log(name); // Sara
+// console.log(age); // 25
+// console.log(gender); // female
