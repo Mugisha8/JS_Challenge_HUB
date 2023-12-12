b1986cf0bdd9b2b9d18776819221302d5857d40b
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-12 23:57:26 +0200

    challenge 139

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1111,3 +1111,23 @@
 
 // // access the elements of a Set
 // console.log(set1.values()); // Set Iterator [1, 2, 3]
+
+// const set1 = new Set([1, 2, 3]);
+
+// // check if an element is in Set
+// console.log(set1.has(1));
+
+// assigning object attributes to variables
+const person = {
+    name: 'Sara',
+    age: 25,
+    gender: 'female'    
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+
+console.log(name); // Sara
+console.log(age); // 25
+console.log(gender); // female
