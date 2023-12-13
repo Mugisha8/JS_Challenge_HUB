b87207e1d7f8da0efb9657ef4b5b3ab42c19bf5d
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-13 23:38:14 +0200

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1149,11 +1149,11 @@
 
 
 // constructor function
-function Person () {
-    this.name = 'John',
-    this.age = 23
-}
+// function Person () {
+//     this.name = 'John',
+//     this.age = 23
+// }
 
-// create an object
-const person1 = new Person();
+// // create an object
+// const person1 = new Person();
     
