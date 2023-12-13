07c89f96b6da3a96e17c594eb64f214d6968acac
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-13 23:44:13 +0200

    challenge 146

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1190,3 +1190,22 @@
 //          return ('Hello' + ' ' + this.name);
 //      }
 //  }
+
+class Person {
+    constructor(name) {
+    this.name = name;
+  }
+
+    // defining method
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+let person1 = new Person('John');
+
+// accessing property
+console.log(person1.name); // John
+
+// accessing method
+person1.greet(); // Hello John
