e0d088a67c698c7cf661f7e4d709205fa262cdc5
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-13 23:41:49 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1166,15 +1166,15 @@
 //   }
 
 // creating a class
-class Person {
-    constructor(name) {
-      this.name = name;
-    }
-  }
+// class Person {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
   
-  // creating an object
-  const person1 = new Person('John');
-  const person2 = new Person('Jack');
+//   // creating an object
+//   const person1 = new Person('John');
+//   const person2 = new Person('Jack');
   
-  console.log(person1.name); // John
-  console.log(person2.name); // Jack
+//   console.log(person1.name); // John
+//   console.log(person2.name); // Jack
