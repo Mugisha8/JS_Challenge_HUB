93e6577ec3e4d20ad45bac6a846c1fb036731d23
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-13 23:46:40 +0200

    challenge 148

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1209,3 +1209,26 @@
 
 // // accessing method
 // person1.greet(); // Hello John
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    // getter
+    get personName() {
+        return this.name;
+    }
+
+    // setter
+    set personName(x) {
+        this.name = x;
+    }
+}
+
+let person1 = new Person('Jack');
+console.log(person1.name); // Jack
+
+// changing the value of name property
+person1.personName = 'Sarah';
+console.log(person1.name); // Sarah
