d0d77372381155e84ac0f04746fff3806bbd343c
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-14 22:50:59 +0200

    challenge 156

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1269,3 +1269,26 @@
 // }
 
 // let p = new Person(); // ReferenceError: Can't find variable: a
+
+
+class Person {}
+console.log(typeof Person); // function
+
+// parent class
+class Person { 
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+// inheriting parent class
+class Student extends Person {
+
+}
+
+let student1 = new Student('Jack');
+student1.greet();
