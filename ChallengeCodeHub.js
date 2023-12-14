d3ff7892d0854faaa39ba8a77983fb81b1b9b17f
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-14 22:51:25 +0200

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1271,24 +1271,24 @@
 // let p = new Person(); // ReferenceError: Can't find variable: a
 
 
-class Person {}
-console.log(typeof Person); // function
+// class Person {}
+// console.log(typeof Person); // function
 
-// parent class
-class Person { 
-    constructor(name) {
-        this.name = name;
-    }
+// // parent class
+// class Person { 
+//     constructor(name) {
+//         this.name = name;
+//     }
 
-    greet() {
-        console.log(`Hello ${this.name}`);
-    }
-}
+//     greet() {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
 
-// inheriting parent class
-class Student extends Person {
+// // inheriting parent class
+// class Student extends Person {
 
-}
+// }
 
-let student1 = new Student('Jack');
-student1.greet();
+// let student1 = new Student('Jack');
+// student1.greet();
