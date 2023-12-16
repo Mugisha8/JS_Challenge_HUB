a65071caf5525adc52e1674859288fc3f8cc59bf
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-16 23:05:35 +0200

    challenge 166

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1510,6 +1510,22 @@
 // console.log(proxy.age); // 24
 // console.log(proxy.class); // property does not exist
 
+let student = {
+    name: 'Jack',
+    age: 24
+}
+
+const handler = {
+
+    // get the object key and value
+    get(obj, prop) {
+
+        return obj[prop];
+  }
+}
+
+const proxy = new Proxy(student, handler);
+console.log(proxy.name); // Jack
 
 
 
