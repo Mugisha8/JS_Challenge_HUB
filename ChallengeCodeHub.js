fe7bf5a351cc8fa66406c662e446ab12e82b95cf
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-16 23:07:53 +0200

    challenge 167

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1527,6 +1527,26 @@
 // const proxy = new Proxy(student, handler);
 // console.log(proxy.name); // Jack
 
+let student = {
+    name: 'John'
+}
+
+let setNewValue = {
+  set: function(obj, prop, value) {
+
+    obj[prop] = value;
+    return;
+  }
+};
+
+// setting new proxy
+let person = new Proxy(student, setNewValue);
+
+// setting new key/value
+person.age = 25;
+console.log(person); // Proxy {name: "John", age: 25}
+
+
 
 
 
