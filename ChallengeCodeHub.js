3773fc5c24b00ac9e9702c7c18d161d85d9eaceb
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-16 23:10:36 +0200

    challenge 168

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1546,7 +1546,28 @@
 // person.age = 25;
 // console.log(person); // Proxy {name: "John", age: 25}
 
+let student = {
+    name: 'Jack',
+    age: 24
+}
 
+const handler = {
+
+    // get the object key and value
+    get(obj, prop) {
+
+    // check condition
+    if (prop == 'name') {
+      return obj[prop];
+    } else {
+      return 'Not allowed';
+    }
+  }
+}
+
+const proxy = new Proxy(student, handler);
+console.log(proxy.name); // Jack
+console.log(proxy.age); // Not allowed
 
 
 
