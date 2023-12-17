71e2945c7147e8491fe8ee36418e8e0d0db5f0c0
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-17 23:56:18 +0200

    component updte

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1571,11 +1571,11 @@
 
 
 // program to display a text using setTimeout method
-function greet() {
-    console.log('Hello world');
-}
+// function greet() {
+//     console.log('Hello world');
+// }
 
-setTimeout(greet, 3000);
-console.log('This message is shown first');
+// setTimeout(greet, 3000);
+// console.log('This message is shown first');
 
 
