a40447128c1da935f8990154e4882969f4087010
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-17 23:55:39 +0200

    challenge 170

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1570,6 +1570,12 @@
 // console.log(proxy.age); // Not allowed
 
 
+// program to display a text using setTimeout method
+function greet() {
+    console.log('Hello world');
+}
 
+setTimeout(greet, 3000);
+console.log('This message is shown first');
 
 
