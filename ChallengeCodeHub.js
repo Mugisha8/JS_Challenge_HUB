eff10c8e5a5b741d17975156416edafb68899619
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-17 23:59:35 +0200

    184

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1627,3 +1627,11 @@
 // // clearTimeout
 // clearTimeout(id); 
 // console.log('setTimeout is stopped.');
+
+// program to display a name
+function greet(name, lastName) {
+    console.log('Hello' + ' ' + name + ' ' + lastName);
+}
+
+// passing argument to setTimeout
+setTimeout(greet, 1000, 'John', 'Doe');
