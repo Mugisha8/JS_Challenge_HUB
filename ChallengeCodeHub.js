db4b0b36f41efcf1e1ae81569523936f697b8786
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-17 23:57:29 +0200

    component update

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1581,12 +1581,12 @@
 
 
 
-// program to display a text using setTimeout method
-function greet() {
-    console.log('Hello world');
-}
+// // program to display a text using setTimeout method
+// function greet() {
+//     console.log('Hello world');
+// }
 
-let intervalId = setTimeout(greet, 3000);
-console.log('Id: ' + intervalId);   
+// let intervalId = setTimeout(greet, 3000);
+// console.log('Id: ' + intervalId);   
 
 
