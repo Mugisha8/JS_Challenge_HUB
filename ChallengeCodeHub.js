e2c5e4da22c531c12c7a190db99e4c6d11683630
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-18 19:13:52 -0300

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1590,22 +1590,22 @@
 
 
 // program to display time every 3 seconds
-function showTime() {
+// function showTime() {
 
-    // return new date and time
-    let dateTime= new Date();
+//     // return new date and time
+//     let dateTime= new Date();
 
-    // returns the current local time
-    let time = dateTime.toLocaleTimeString();
+//     // returns the current local time
+//     let time = dateTime.toLocaleTimeString();
 
-    console.log(time)
+//     console.log(time)
 
-    // display the time after 3 seconds
-     setTimeout(showTime, 3000);
-}
+//     // display the time after 3 seconds
+//      setTimeout(showTime, 3000);
+// }
 
-// calling the function
-showTime();
+// // calling the function
+// showTime();
 
 
 
