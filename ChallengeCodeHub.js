5a3eb177f55d0332e78ecf05ba72d53e8fa3ccd2
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-18 19:11:44 -0300

    challenge 182

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1572,13 +1572,15 @@
 //***************************************************************************************** */
 
 
-// program to display a text using setTimeout method
-function greet() {
-    console.log('Hello world');
-}
+// // program to display a text using setTimeout method
+// function greet() {
+//     console.log('Hello world');
+// }
 
-setTimeout(greet, 3000);
-console.log('This message is shown first');
+// setTimeout(greet, 3000);
+// console.log('This message is shown first');
+
+
 
 
 
