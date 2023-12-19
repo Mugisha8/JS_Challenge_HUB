376fc77c1af526c6c1d78adcd5d481665d195b03
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-19 23:01:00 +0200

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1636,18 +1636,18 @@
 
 
 // function
-function greet(name, callback) {
-    console.log('Hi' + ' ' + name);
-    callback();
-}
+// function greet(name, callback) {
+//     console.log('Hi' + ' ' + name);
+//     callback();
+// }
 
-// callback function
-function callMe() {
-    console.log('I am callback function');
-}
+// // callback function
+// function callMe() {
+//     console.log('I am callback function');
+// }
 
-// passing function as an argument
-greet('Peter', callMe);
+// // passing function as an argument
+// greet('Peter', callMe);
 
 
 
