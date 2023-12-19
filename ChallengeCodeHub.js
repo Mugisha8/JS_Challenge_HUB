d11532a7f9905c8501e6248c49df85830d27da25
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-19 23:00:27 +0200

    challenge 201

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1635,8 +1635,19 @@
 // setTimeout(greet, 1000, 'John', 'Doe');
 
 
+// function
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    callback();
+}
 
-// 
+// callback function
+function callMe() {
+    console.log('I am callback function');
+}
+
+// passing function as an argument
+greet('Peter', callMe);
 
 
 
