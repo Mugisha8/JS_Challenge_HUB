db3288840c695af3e018e9b5da969ca78bc23b1a
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-20 23:27:37 +0200

    challenge 210

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1647,6 +1647,19 @@
 // greet("peter",eden)
 
 
+const count = true;
+
+let countValue = new Promise(function (resolve, reject) {
+    if (count) {
+        resolve("There is a count value.");
+    } else {
+        reject("There is no count value");
+    }
+});
+
+console.log(countValue);
+
+
 
 
 
