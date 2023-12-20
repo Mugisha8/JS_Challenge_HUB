67c835508eb5b71ae57c7d2d7fc6e617568f95cb
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-20 23:30:47 +0200

    component updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1663,20 +1663,20 @@
 
 // returns a promise
 
-let countValue = new Promise(function (resolve, reject) {
-    resolve("Promise resolved");
-  });
+// let countValue = new Promise(function (resolve, reject) {
+//     resolve("Promise resolved");
+//   });
   
-  // executes when promise is resolved successfully
+//   // executes when promise is resolved successfully
   
-  countValue
-    .then(function successValue(result) {
-      console.log(result);
-    })
+//   countValue
+//     .then(function successValue(result) {
+//       console.log(result);
+//     })
   
-    .then(function successValue1() {
-      console.log("You can call multiple functions this way.");
-    });
+//     .then(function successValue1() {
+//       console.log("You can call multiple functions this way.");
+//     });
 
 
 
