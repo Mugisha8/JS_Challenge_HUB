fc21c9483b057e51cb0b5752c101a9eaaae19b51
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-20 23:36:13 +0200

    challenge udpated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1716,18 +1716,18 @@
 // });
 
 
-// returns a promise
-let countValue = new Promise(function (resolve, reject) {
-    // could be resolved or rejected   
-    resolve('Promise resolved'); 
-});
+// // returns a promise
+// let countValue = new Promise(function (resolve, reject) {
+//     // could be resolved or rejected   
+//     resolve('Promise resolved'); 
+// });
 
-// add other blocks of code
-countValue.finally(
-    function greet() {
-        console.log('This code is executed.');
-    }
-);
+// // add other blocks of code
+// countValue.finally(
+//     function greet() {
+//         console.log('This code is executed.');
+//     }
+// );
 
 
 
