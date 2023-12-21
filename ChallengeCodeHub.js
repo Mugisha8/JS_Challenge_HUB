505116080e467a06a84181592763bc32ff12def1
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-21 18:53:38 -0300

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1743,9 +1743,9 @@
 
 // async function example
 
-async function f() {
-    console.log('Async function.');
-    return Promise.resolve(1);
-}
+// async function f() {
+//     console.log('Async function.');
+//     return Promise.resolve(1);
+// }
 
-f(); 
+// f(); 
