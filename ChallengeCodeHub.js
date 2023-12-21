668340a9d1debbc95eb9859d8f07d2a6b027e6d4
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-21 18:57:36 -0300

    challenge 224

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1743,6 +1743,29 @@
 //     console.log(result1);
 // }
 
+// a promise
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+    resolve('Promise resolved')}, 4000); 
+});
+
+// async function
+async function asyncFunc() {
+    try {
+        // wait until the promise resolves 
+        let result = await promise; 
+
+        console.log(result);
+    }   
+    catch(error) {
+        console.log(error);
+    }
+}
+
+// calling the async function
+asyncFunc(); // Promise resolved
+
+
 
 
 
