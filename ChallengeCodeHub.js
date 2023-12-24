0ea3a42e7e25cda9ccb9d47c049eca072666ce41
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-24 19:07:00 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1838,22 +1838,22 @@
 
 // program to stop the setInterval() method after five times
 
-let count = 0;
+// let count = 0;
 
-// function creation
-let interval = setInterval(function(){
+// // function creation
+// let interval = setInterval(function(){
 
-    // increasing the count by 1
-    count += 1;
+//     // increasing the count by 1
+//     count += 1;
 
-    // when count equals to 5, stop the function
-    if(count === 5){
-        clearInterval(interval);
-    }
+//     // when count equals to 5, stop the function
+//     if(count === 5){
+//         clearInterval(interval);
+//     }
 
-    // display the current time
-    let dateTime= new Date();
-    let time = dateTime.toLocaleTimeString();
-    console.log(time);
+//     // display the current time
+//     let dateTime= new Date();
+//     let time = dateTime.toLocaleTimeString();
+//     console.log(time);
 
-}, 2000);
+// }, 2000);
