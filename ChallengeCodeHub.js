b8c8e3ad4e9b29aabba7164222e359736a192d60
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-24 19:09:37 +0200

    challenge 236

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1859,3 +1859,10 @@
 // }, 2000);
 
 
+// program to display a name
+function greet(name, lastName) {
+    console.log('Hello' + ' ' + name + ' ' + lastName);
+}
+
+// passing argument to setInterval
+setInterval(greet, 1000, 'John', 'Doe');
