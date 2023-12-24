1cb9a86526a12d95895a682f143efefba5c43af9
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-24 19:05:38 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1822,15 +1822,15 @@
 // 
 
 // program to display time every 5 seconds
-function showTime() {
+// function showTime() {
 
-    // return new date and time
-    let dateTime= new Date();
+//     // return new date and time
+//     let dateTime= new Date();
 
-    // return the time
-    let time = dateTime.toLocaleTimeString();
+//     // return the time
+//     let time = dateTime.toLocaleTimeString();
 
-    console.log(time)
-}
+//     console.log(time)
+// }
 
-let display = setInterval(showTime, 5000);
+// let display = setInterval(showTime, 5000);
