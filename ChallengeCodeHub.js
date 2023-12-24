83f6c82b6c5e4ec6144373dc346f04b4fac0b821
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-24 19:18:19 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1867,22 +1867,22 @@
 // // passing argument to setInterval
 // setInterval(greet, 1000, 'John', 'Doe');
 
-let student = {
-    name: 'Jack',
-    age: 23
-}
+// let student = {
+//     name: 'Jack',
+//     age: 23
+// }
 
-const handler = {
-    set: function (obj, prop, value) {
-        if (obj[prop]) {
+// const handler = {
+//     set: function (obj, prop, value) {
+//         if (obj[prop]) {
             
-            // cannot change the student value
-            console.log('Read only')
-        }
-    }
-};
+//             // cannot change the student value
+//             console.log('Read only')
+//         }
+//     }
+// };
 
-const proxy = new Proxy(student, handler);
+// const proxy = new Proxy(student, handler);
 
-proxy.name = 'John'; // Read only
-proxy.age = 33; // Read only
+// proxy.name = 'John'; // Read only
+// proxy.age = 33; // Read only
