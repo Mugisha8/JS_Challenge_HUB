fcf068246aaac0e2a28604fce0c3d1740fef27c2
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-25 20:51:49 +0200

    challenge 247

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1924,6 +1924,32 @@
 
 // const date4 = new Date("JULY, 1, 2020");
 // console.log(date4); // Wed Jul 01 2020 00:00:00
+
+const timeInMilliseconds = Date.now();
+console.log(timeInMilliseconds); // 1593765214488
+
+const time = new Date;
+
+// get day of the month
+const date = time.getDate();
+console.log(date); // 30
+
+// get day of the week
+const year = time.getFullYear();
+console.log(year); // 2020
+
+const utcDate = time.getUTCDate();
+console.log(utcDate); // 30
+
+const event = new Date('Feb 19, 2020 23:15:30');
+// set the date
+event.setDate(15);
+console.log(event.getDate()); // 15
+
+// Only 28 days in February!
+event.setDate(35);
+
+console.log(event.getDate()); // 7
  
 
 
