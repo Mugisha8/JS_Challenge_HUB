906b0851e29b5a4ec1e6b22466d51ef149033b5d
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-25 20:48:32 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1905,6 +1905,9 @@
 // const date1 = new Date("2020");
 // console.log(date1); // Wed Jul 01 2020 05:45:00 GMT+0545
 
+const date = new Date("2020-07-01T12:00:00Z");
+console.log(date); // Wed Jul 01 2020 17:45:00 GMT+0545
+
 
 
 
