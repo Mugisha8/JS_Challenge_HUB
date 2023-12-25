e5ab6fdfea2ac5bca50c4e94ceaca79ee591b977
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-25 20:26:53 +0200

    challenge 237

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1886,3 +1886,18 @@
 
 // proxy.name = 'John'; // Read only
 // proxy.age = 33; // Read only
+
+
+
+
+
+
+// ISO Date(International Standard)
+const date = new Date("2020-07-01");
+
+// the result date will be according to UTC
+console.log(date); // Wed Jul 01 2020 05:45:00 GMT+0545
+
+
+
+
