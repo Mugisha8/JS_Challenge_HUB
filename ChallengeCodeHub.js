86163a1e71f87d2777eda5b49de397d94557e727
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-25 20:47:13 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1893,10 +1893,13 @@
 
 
 // ISO Date(International Standard)
-const date = new Date("2020-07-01");
+// const date = new Date("2020-07-01");
 
-// the result date will be according to UTC
-console.log(date); // Wed Jul 01 2020 05:45:00 GMT+0545
+// // the result date will be according to UTC
+// console.log(date); // Wed Jul 01 2020 05:45:00 GMT+0545
+
+
+
 
 
 
