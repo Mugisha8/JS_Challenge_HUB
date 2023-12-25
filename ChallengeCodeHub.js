d237b6023a2447b896448235b4e36edfb8c79ed9
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-25 20:54:00 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1956,12 +1956,12 @@
 
 
 
-const currentDate = new Date();
-const date = currentDate.getDate();
-const month = currentDate.getMonth();
-const year = currentDate.getFullYear();
+// const currentDate = new Date();
+// const date = currentDate.getDate();
+// const month = currentDate.getMonth();
+// const year = currentDate.getFullYear();
 
-// show in specific format
-let monthDateYear = (month+1) + '/' + date + '/' + year;
+// // show in specific format
+// let monthDateYear = (month+1) + '/' + date + '/' + year;
 
-console.log(monthDateYear); // 7/3/2020
+// console.log(monthDateYear); // 7/3/2020
