a71e6c01745f6533ab3dd17c0b82d3ef3cd749b6
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-26 15:33:36 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -1960,28 +1960,28 @@
 
 
 
-const person = {
-    name : 'Jack',
-    age: 25,
+// const person = {
+//     name : 'Jack',
+//     age: 25,
 
-    // this inside method
-    // this refers to the object itself
-    greet() {
-        console.log(this);        // {name: "Jack", age ...}
-        console.log(this.age);  // 25
+//     // this inside method
+//     // this refers to the object itself
+//     greet() {
+//         console.log(this);        // {name: "Jack", age ...}
+//         console.log(this.age);  // 25
 
-        // inner function
-        function innerFunc() {
+//         // inner function
+//         function innerFunc() {
         
-            // this refers to the global object
-            console.log(this);       // Window { ... }
-            console.log(this.age);    // undefined
+//             // this refers to the global object
+//             console.log(this);       // Window { ... }
+//             console.log(this.age);    // undefined
             
-        }
+//         }
 
-        innerFunc();
+//         innerFunc();
 
-    }
-}
+//     }
+// }
 
-person.greet();
+// person.greet();
