98da815b4df9e613cf8d798b77f861bba3b52633
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-27 16:20:45 +0200

    challenge 280

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2067,4 +2067,13 @@
 // hello();
 
 
+'use strict';
+
+let obj = {};
+Object.preventExtensions(obj);
+
+// Assignment to a new property on a non-extensible object
+obj.newValue = 'new value'; // throws an error
+
+
 
