fc8d66840a7c9625b9ab29db10dfbab420d1bddf
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-29 19:08:24 -0300

    chalenge udpated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2104,13 +2104,13 @@
 //     console.log(n);
 // }
 
-const arr = ['h', 'e', 'l', 'l', 'o'];
+// const arr = ['h', 'e', 'l', 'l', 'o'];
 
-let arrIterator = arr[Symbol.iterator]();
+// let arrIterator = arr[Symbol.iterator]();
 
-console.log(arrIterator.next()); // {value: "h", done: false}
-console.log(arrIterator.next()); // {value: "e", done: false}
-console.log(arrIterator.next()); // {value: "l", done: false}
-console.log(arrIterator.next()); // {value: "l", done: false}
-console.log(arrIterator.next()); // {value: "o", done: false}
-console.log(arrIterator.next()); // {value: undefined, done: true}
+// console.log(arrIterator.next()); // {value: "h", done: false}
+// console.log(arrIterator.next()); // {value: "e", done: false}
+// console.log(arrIterator.next()); // {value: "l", done: false}
+// console.log(arrIterator.next()); // {value: "l", done: false}
+// console.log(arrIterator.next()); // {value: "o", done: false}
+// console.log(arrIterator.next()); // {value: undefined, done: true}
