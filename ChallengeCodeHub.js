cc74a21a4df363e526937ea960876f376fef028e
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-29 19:10:21 -0300

    challenge 304

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2120,3 +2120,39 @@
 // for (let i of arr) {
 //     console.log(i);
 // }
+
+function displayElements(arr) {
+
+    // to update the iteration
+    let n = 0;
+
+    return {
+
+        // implementing the next() function
+        next() {
+
+            if(n < arr.length) {
+                return {
+                    value: arr[n++],
+                    done: false
+                }
+            }
+
+            return {
+                value: undefined,
+                done: true
+            }
+        }
+    }
+}
+
+const arr = ['h', 'e', 'l', 'l', 'o'];
+
+const arrIterator = displayElements(arr);
+
+console.log(arrIterator.next());
+console.log(arrIterator.next());
+console.log(arrIterator.next());
+console.log(arrIterator.next());
+console.log(arrIterator.next());
+console.log(arrIterator.next());
