fe04f4978bd6b42845643817b7895cdedb35783e
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-29 19:12:25 -0300

    challenge 306

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2156,3 +2156,18 @@
 // console.log(arrIterator.next());
 // console.log(arrIterator.next());
 // console.log(arrIterator.next());
+
+// generator function
+function* generatorFunc() {
+
+    console.log("1. code before the first yield");
+    yield 100;
+    
+   console.log("2. code before the second yield");
+    yield 200;
+}
+
+// returns generator object
+const generator = generatorFunc();
+
+console.log(generator.next());
