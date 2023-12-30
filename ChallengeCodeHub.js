8d41584390d02506e417a6f30663d72ba81495c9
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-30 13:10:58 +0200

    challenge udpated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2281,20 +2281,20 @@
 // console.log(result3); // world hello hello
 
 
-const string = 'Hello hello hello';
+// const string = 'Hello hello hello';
 
-// performing a replacement
-const result1 = string.replace(/hello/, 'world');
-console.log(result1); // Hello world hello
+// // performing a replacement
+// const result1 = string.replace(/hello/, 'world');
+// console.log(result1); // Hello world hello
 
-// performing global replacement
-const result2 = string.replace(/hello/g, 'world');
-console.log(result2); // Hello world world
+// // performing global replacement
+// const result2 = string.replace(/hello/g, 'world');
+// console.log(result2); // Hello world world
 
-// performing case-insensitive replacement
-const result3 = string.replace(/hello/i, 'world');
-console.log(result3); // world hello hello
+// // performing case-insensitive replacement
+// const result3 = string.replace(/hello/i, 'world');
+// console.log(result3); // world hello hello
 
-// performing global case-insensitive replacement
-const result4 = string.replace(/hello/gi, 'world');
-console.log(result4); // world world world
+// // performing global case-insensitive replacement
+// const result4 = string.replace(/hello/gi, 'world');
+// console.log(result4); // world world world
