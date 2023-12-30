6ded14e2164859ff50d152ce5b2a7289f7a4afd0
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-30 13:09:51 +0200

    challenge 358

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2266,3 +2266,16 @@
 // const result2 = string.replace(/hello/g, 'world');
 // console.log(result2); // Hello world world
 
+const string = 'Hello hello hello';
+
+// performing a replacement
+const result1 = string.replace(/hello/, 'world');
+console.log(result1); // Hello world hello
+
+// performing global replacement
+const result2 = string.replace(/hello/g, 'world');
+console.log(result2); // Hello world world
+
+// performing case-insensitive replacement
+const result3 = string.replace(/hello/i, 'world');
+console.log(result3); // world hello hello
