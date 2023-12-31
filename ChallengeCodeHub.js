387f80002bbc9dc505c5fa09e428f2e03d3e437a
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-31 13:18:53 +0200

    last JS challenge of 2023

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2349,23 +2349,23 @@
 // validateEmail(email);
 
 
-const string = 'Find me';
-const pattern = /me/;
+// const string = 'Find me';
+// const pattern = /me/;
 
-// search if the pattern is in string variable
-const result1 = string.search(pattern);
-console.log(result1); // 5
+// // search if the pattern is in string variable
+// const result1 = string.search(pattern);
+// console.log(result1); // 5
 
-// replace the character with another character
-const string1 = 'Find me';
-string1.replace(pattern, 'found you'); // Find found you
+// // replace the character with another character
+// const string1 = 'Find me';
+// string1.replace(pattern, 'found you'); // Find found you
 
-// splitting strings into array elements
-const regex1 = /[\s,]+/;
-const result2 = 'Hello world! '.split(regex1);
-console.log(result2); // ['Hello', 'world!', '']
+// // splitting strings into array elements
+// const regex1 = /[\s,]+/;
+// const result2 = 'Hello world! '.split(regex1);
+// console.log(result2); // ['Hello', 'world!', '']
 
-// searching the phone number pattern
-const regex2 = /(\d{3})\D(\d{3})-(\d{4})/g;
-const result3 = regex2.exec('My phone number is: 555 123-4567.');
-console.log(result3); // ["555 123-4567", "555", "123", "4567"]
+// // searching the phone number pattern
+// const regex2 = /(\d{3})\D(\d{3})-(\d{4})/g;
+// const result3 = regex2.exec('My phone number is: 555 123-4567.');
+// console.log(result3); // ["555 123-4567", "555", "123", "4567"]
