5e406868358e7009450243f6a1b546773b76e965
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-31 13:04:07 +0200

    challenge 353

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2213,3 +2213,19 @@
 // const string1 = 'Find me';
 // string1.replace(pattern, 'found you'); // Find found you
 
+const string = 'Find me';
+const pattern = /me/;
+
+// search if the pattern is in string variable
+const result1 = string.search(pattern);
+console.log(result1); // 5
+
+// replace the character with another character
+const string1 = 'Find me';
+string1.replace(pattern, 'found you'); // Find found you
+
+// splitting strings into array elements
+const regex1 = /[\s,]+/;
+const result2 = 'Hello world! '.split(regex1);
+console.log(result2); // ['Hello', 'world!', '']
+
