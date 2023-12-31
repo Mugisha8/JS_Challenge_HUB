ce2f3fcc75d924d253633dbdbf0dc7d19a0e7845
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-31 13:16:05 +0200

    challenge udpated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2327,23 +2327,23 @@
 
 // program to validate the email address
 
-function validateEmail(email) {
+// function validateEmail(email) {
 
-    // regex pattern for email
-    const re = /\S+@\S+\.\S+/g;
+//     // regex pattern for email
+//     const re = /\S+@\S+\.\S+/g;
 
-    // check if the email is valid
-    let result = re.test(email);
-    if (result) {
-        console.log('The email is valid.');
-    }
-    else {
-        let newEmail = prompt('Enter a valid email:');
-        validateEmail(newEmail);
-    }
-}
+//     // check if the email is valid
+//     let result = re.test(email);
+//     if (result) {
+//         console.log('The email is valid.');
+//     }
+//     else {
+//         let newEmail = prompt('Enter a valid email:');
+//         validateEmail(newEmail);
+//     }
+// }
 
-// take input
-let email = prompt('Enter an email: ');
+// // take input
+// let email = prompt('Enter an email: ');
 
-validateEmail(email);
+// validateEmail(email);
