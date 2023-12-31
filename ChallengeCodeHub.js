515f806e664572d62690e0bbc288b9fef6d9c068
Mugisha8 <yvesmugisha8@gmail.com>
2023-12-31 13:15:22 +0200

    challenge 362

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2323,3 +2323,27 @@
 // // // performing global case-insensitive replacement
 // // const result4 = string.replace(/hello/gi, 'world');
 // // console.log(result4); // world world world
+
+
+// program to validate the email address
+
+function validateEmail(email) {
+
+    // regex pattern for email
+    const re = /\S+@\S+\.\S+/g;
+
+    // check if the email is valid
+    let result = re.test(email);
+    if (result) {
+        console.log('The email is valid.');
+    }
+    else {
+        let newEmail = prompt('Enter a valid email:');
+        validateEmail(newEmail);
+    }
+}
+
+// take input
+let email = prompt('Enter an email: ');
+
+validateEmail(email);
