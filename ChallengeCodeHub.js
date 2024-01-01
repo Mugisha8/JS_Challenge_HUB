986a7ad117737c4198968f3b5d4fe00528f2b09c
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-01 17:24:05 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2485,27 +2485,27 @@
 
 // console.log(student.firstName); // Sarah
 
-const student = {
-    firstName: 'Monica'
-}
+// const student = {
+//     firstName: 'Monica'
+// }
 
-// getting property
-Object.defineProperty(student, "getName", {
-    get : function () {
-        return this.firstName;
-    }
-});
+// // getting property
+// Object.defineProperty(student, "getName", {
+//     get : function () {
+//         return this.firstName;
+//     }
+// });
 
-// setting property
-Object.defineProperty(student, "changeName", {
-    set : function (value) {
-        this.firstName = value;
-    }
-});
+// // setting property
+// Object.defineProperty(student, "changeName", {
+//     set : function (value) {
+//         this.firstName = value;
+//     }
+// });
 
-console.log(student.firstName); // Monica
+// console.log(student.firstName); // Monica
 
-// changing the property value
-student.changeName = 'Sarah';
+// // changing the property value
+// student.changeName = 'Sarah';
 
-console.log(student.firstName); // Sarah
+// console.log(student.firstName); // Sarah
