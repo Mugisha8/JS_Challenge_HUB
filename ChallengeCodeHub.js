1a4aff0e6bd44094e1c06f6b857da563233d03c9
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-01 17:15:29 +0200

    challenge updated

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2429,12 +2429,12 @@
 // student.greet(); // hello
 
 
-const person = {
-    name: 'John',
-    age: 30,
+// const person = {
+//     name: 'John',
+//     age: 30,
 
-    // accessing name property by using this.name
-    greet: function() { console.log('The name is' + ' ' + this.name); }
-};
+//     // accessing name property by using this.name
+//     greet: function() { console.log('The name is' + ' ' + this.name); }
+// };
 
-person.greet();
+// person.greet();
