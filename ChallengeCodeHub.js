942a4f03ec4df8e94b4b05db3782565c2d80309c
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-01 17:16:07 +0200

    challenge 412

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2438,3 +2438,13 @@
 // };
 
 // person.greet();
+
+const person = {
+    name: 'John',
+    age: 30,
+    greet: function() {
+        let surname = 'Doe';
+        console.log('The name is' + ' ' + this.name + ' ' + surname); }
+};
+
+person.greet();
