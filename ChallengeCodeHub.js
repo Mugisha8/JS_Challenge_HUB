e3d8ad58b51d589d274276772733edd99022af79
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-01 17:17:29 +0200

    challenge updted

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2449,22 +2449,22 @@
 
 // person.greet();
 
-const student = {
+// const student = {
 
-    // data property
-    firstName: 'Monica',
+//     // data property
+//     firstName: 'Monica',
     
-    // accessor property(getter)
-    get getName() {
-        return this.firstName;
-    }
-};
+//     // accessor property(getter)
+//     get getName() {
+//         return this.firstName;
+//     }
+// };
 
-// accessing data property
-console.log(student.firstName); // Monica
+// // accessing data property
+// console.log(student.firstName); // Monica
 
-// accessing getter methods
-console.log(student.getName); // Monica
+// // accessing getter methods
+// console.log(student.getName); // Monica
 
-// trying to access as a method
-console.log(student.getName()); // error
+// // trying to access as a method
+// console.log(student.getName()); // error
