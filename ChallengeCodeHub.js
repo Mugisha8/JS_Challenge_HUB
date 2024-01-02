be5d952c8eb99571d69eb676f4334b78553a1c30
Mugisha8 <yvesmugisha8@gmail.com>
2024-01-02 17:02:03 +0200

    challenge 390

--- ChallengeCodeHub.js
+++ ChallengeCodeHub.js
@@ -2413,3 +2413,17 @@
 // let result = parseInt(number);
 
 // console.log(result); // 23
+
+// creating an object
+let student = { };
+
+// adding a property
+student.name = 'John';
+
+// adding a method
+student.greet = function() {
+    console.log('hello');
+}
+
+// accessing a method
+student.greet(); // hello
